555d0d8de87a40672b7df822573466f1ad4a3b61
kmfwn01 <haqimaaziz@gmail.com>
2020-06-15 22:42:23 +0700

    aku nambah file baru menu_program

---
+++ menu_program.c
@@ -0,0 +1,62 @@
+/**
+Nama: Kurnia Ramadhan Putra
+Tanggal: 19-02-2020
+Nama Program: menu_program.c
+*/
+#include <stdio.h>
+
+double tambah(double a, double b) {
+ return a + b;
+}
+
+double kurang(double a, double b) {
+ return a - b;
+}
+double kali(double a, double b) {
+ return a * b;
+}
+double bagi (double a, double b){
+    return a / b;
+    }
+
+
+int main() {
+ int pilihan;
+ double angka1, angka2;
+
+ printf("Menu Program\n");
+ printf("----------------------\n");
+ printf("1. pertambahan\n");
+ printf("2. pengurangan\n");
+ printf("3. perkalian\n");
+ printf("4. pembagian\n");
+
+ printf("Masukkan pilihan Anda : ");
+ scanf("%d", &pilihan);
+
+ printf("Masukkan Angka I : ");
+ scanf("%lf",&angka1);
+ printf("Masukkan Angka II : ");
+ scanf("%lf",&angka2);
+
+ switch (pilihan)
+ {
+  case 1:
+   printf("Hasil penjumlahan : ");
+   printf("%.1f\n", tambah(angka1, angka2));
+  break;
+  case 2:
+   printf("Hasil pengurangan : ");
+   printf("%.1f\n", kurang(angka1, angka2));
+  break;
+  case 3:
+   printf("Hasil perkalian : ");
+   printf("%.1f\n", kali(angka1, angka2));
+  break;
+  case 4:
+            printf("Hasil pembagian : ");
+            printf("%.1f\n", bagi(angka1, angka2));
+        break;
+ }
+ return 0;
+}
