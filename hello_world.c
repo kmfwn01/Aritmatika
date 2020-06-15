73f6848da50117163eb9bf76542bb4b29bb952f9
kmfwn01 <haqimaaziz@gmail.com>
2020-06-15 11:00:32 +0700

    hello world

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+/**
+Nama: Kurnia Ramadhan Putra
+Tanggal: 19-02-2020
+Nama Program: hello_world.c
+*/
+#include <stdio.h>
+
+int main() {
+
+
+	printf("HELLO WORLD");
+	printf("----------------------\n");
+}
