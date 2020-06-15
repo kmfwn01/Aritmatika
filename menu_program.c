b3745727826998698dd35545b17645ee85531c9a
kmfwn01 <haqimaaziz@gmail.com>
2020-06-15 14:17:42 +0700

    CEK WOE

--- menu_program.c
+++ menu_program.c
@@ -8,6 +8,10 @@ Nama Program: menu_program.c
 double tambah(double a, double b) {
 	return a + b;
 }
+daouble mod_dari_kima(double a,double b){
+	return a % b;
+}
+
 
 int main() {
 	int pilihan;
@@ -16,6 +20,8 @@ int main() {
 	printf("Menu Program\n");
 	printf("----------------------\n");
 	printf("1. Tambah\n");
+	printf("2. Mod\n");
+	printf("CEK WOE");
 
 	printf("Masukkan pilihan Anda : ");
 	scanf("%d", &pilihan);
@@ -31,6 +37,11 @@ int main() {
 			printf("Hasil penjumlahan : ");
 			printf("%.1f\n", tambah(angka1, angka2));
 		break;
+
+		case 2:
+			printf("Hasil peembagian : ");
+			printf("%.1f\n", mod(angka1, angka2));
+		break;	
 	}
 	return 0;
 }
